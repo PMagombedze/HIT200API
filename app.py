4eeca878e2df906d4af55ab15af0deddf32f63ed
PMagombedze <percymagoras@gmail.com>
2024-12-14 13:10:56 +0200

    Add initial HTML templates and static files for user authentication and dashboard

--- app.py
+++ app.py
@@ -24,6 +24,44 @@ app.register_blueprint(notifications, url_prefix='/api')
 app.register_blueprint(products, url_prefix='/api')
 
 
+# routes
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/a/dashboard')
+def admin_dashboard():
+    return render_template('admin/dashboard.html')
+
+@app.route('/u/dashboard')
+def user_dashboard():
+    return render_template('user/dashboard.html')
+
+@app.route('/admin/login')
+def admin_login():
+    return render_template('admin/login.html')
+
+@app.route('/login')
+def user_login():
+    return render_template('user/login.html')
+
+@app.route('/register')
+def user_register():
+    return render_template('user/signup.html')
+
+@app.route('/otp')
+def otp():
+    return render_template('user/otp.html')
+
+@app.route('/forgot_password')
+def forgot_password():
+    return render_template('user/forgotpass.html')
+
+@app.route('/reset_password')
+def reset_password():
+    return render_template('user/resetpass.html')
+
+
 with app.app_context():
     db.init_app(app)
     db.create_all()
