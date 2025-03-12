1292b273658a1332af07643dc5a9dba3f1568731
E-I-P <takuehope@gmail.com>
2025-03-12 11:57:03 +0200

    Implement scraping of different sites(in progress)

--- scraper/zimall/scraper.py
+++ scraper/zimall/scraper.py
@@ -32,3 +32,32 @@ for product in soup.find_all('li', {'class': 'ajax_block_product'}):
 
 with open('electronics_zimall.json', 'w') as f:
     json.dump(products, f, indent=4)
+
+
+client = ScrapingBeeClient(api_key=os.environ.get('SCRAPINGBEE_API_KEY'))
+response = client.get('https://www.dailysale.co.zw/categories/tv-audio-video/all')
+
+soup = BeautifulSoup(response.content, 'html.parser')
+products = []
+
+for product in soup.find_all('li', {'class': 'ajax_block_product'}):
+    product_block = product.find('div', {'class': 'product-block'})
+    name = product_block.find('h4', {'class': 'name'}).text.strip()
+    price = product_block.find('span', {'itemprop': 'price'}).text.strip()
+    model = product_block.find('meta', {'itemprop': 'Model'})['content']
+    brand = product_block.find('meta', {'itemprop': 'Brand'})['content']
+    description = product_block.find('div', {'class': 'product-desc'}).text.strip()
+    image = product_block.find('img', {'class': 'replace-2x'})['src']
+
+    products.append({
+        'name': name,
+        'price': price,
+        'model': model,
+        'brand': brand,
+        'description': description,
+        'url': image,
+        'store': 'zimall'
+    })
+
+with open('tv_audio_video_json.json', 'w') as f:
+    json.dump(products, f, indent=4)
